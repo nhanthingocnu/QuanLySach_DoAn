b008659932dee8c0c5de30d555235d44d9f510ed
ngocnu <nhanngocnu2004@gmail.com>
2025-12-04 16:05:40 +0700

    Move source code to root folder

---
+++ QuanLySach_DoAn/TrangChu.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using XAML_KHUNG_QLSach;
+
+namespace QuanLySach_DoAn
+{
+    /// <summary>
+    /// Interaction logic for TrangChu.xaml
+    /// </summary>
+    public partial class TrangChu : Window
+    {
+        public TrangChu()
+        {
+            InitializeComponent(); 
+        }
+        
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            DangNhap login = new DangNhap();
+            login.Show();
+            this.Close(); 
+        }
+
+        
+        private void btnMain_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
